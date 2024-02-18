2fcb56118dc282c8f0ac1247e91527467a554958
reiya.yoshiaga@reirei-anpan <bestkimrist@gmail.com>
2024-02-19 02:15:15 +0900

    シークレットキーアップデート

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require __DIR__ . '/vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello actions");
+    $response->getBody()->write("Hello Actions");
     return $response;
 });
 
